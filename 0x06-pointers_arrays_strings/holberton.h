a5eb9cf56f3a2fd1e7a742d0a672bc529315543d
Ayinde John <lolaayinde@gmail.com>
2022-04-25 04:52:41 +0100

    updated file

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -6,3 +6,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *s);
+char *infinite_add(char *n1, char *n2 , char *r, int size_r);
+void print_number(int n)
