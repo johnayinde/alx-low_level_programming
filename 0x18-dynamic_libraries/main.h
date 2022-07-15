d61ac6cca5b57c985eb3e36f9e56f338ed574bc1
Ayinde John <lolaayinde@gmail.com>
2022-07-15 10:58:36 +0100

    changed files

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef __MAIN__
-#define __MAIN__
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,7 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int nrandom();
 
-
-#endif /* __MAIN__ */
+#endif
